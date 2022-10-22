dd27b0605556cbe99b8fcec04f1f263c4482ff81
Aaron Greiner <aarongreiner@icloud.com>
2022-10-22 14:09:56 +0200

    init day 1

---
+++ AoC_Day_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace AoC_Day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = File.ReadAllText(@"../../input.txt");
+            char[] array = input.ToCharArray();
+            int floor = 0;
+            bool first_basement = false;
+            int count_steps = 0;
+
+            foreach (char i in array)
+            {
+                count_steps++;
+
+                switch (i)
+                {
+                    case '(':
+                        floor++;
+                        break;
+                    case ')':
+                        floor--;
+                        break;
+                }
+
+                if (!first_basement && floor < 0)
+                {
+                    first_basement = true;
+                    Console.WriteLine("First Basement: " + count_steps);
+                }
+            }
+
+            Console.WriteLine("End Floor: " + floor);
+            Console.ReadLine();
+        }
+    }
+}
