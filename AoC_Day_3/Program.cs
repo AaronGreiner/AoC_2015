92340c69daf06b0bf8e0e6edc516801f5bccac5c
Aaron Greiner <aarongreiner@icloud.com>
2022-10-23 13:44:57 +0200

    day 3 bonus

--- AoC_Day_3/Program.cs
+++ AoC_Day_3/Program.cs
@@ -12,43 +12,64 @@ namespace AoC_Day_3
         {
             string input = File.ReadAllText(@"../../input.txt");
             char[] array = input.ToCharArray();
-            int x = 0;
-            int y = 0;
-
-            List<Point> locations = new List<Point>();
-            locations.Add(new Point(x, y));
+            Point loc = new Point(0, 0);
+            
             List<Point> delivered_houses = new List<Point>();
-            delivered_houses.Add(new Point(x, y));
+            delivered_houses.Add(loc);
+
+            bool santas_turn = true;
+            Point loc_santa = new Point(0, 0);
+            Point loc_robo = new Point(0, 0);
+            Point loc_current = new Point(0, 0);
+            List<Point> delivered_houses_bonus = new List<Point>();
+            delivered_houses_bonus.Add(loc_current);
 
             foreach (char i in array)
             {
+                loc_current = santas_turn ? loc_santa : loc_robo;
+
                 switch (i)
                 {
                     case '^':
-                        y++;
+                        loc.Y++;
+                        loc_current.Y++;
                         break;
                     case 'v':
-                        y--;
+                        loc.Y--;
+                        loc_current.Y--;
                         break;
                     case '>':
-                        x++;
+                        loc.X++;
+                        loc_current.X++;
                         break;
                     case '<':
-                        x--;
+                        loc.X--;
+                        loc_current.X--;
                         break;
                 }
 
-                Point p = new Point(x, y);
-
-                locations.Add(p);
-
-                if (!delivered_houses.Contains(p))
+                if (!delivered_houses.Contains(loc))
                 {
-                    delivered_houses.Add(p);
+                    delivered_houses.Add(loc);
                 }
+
+                if (!delivered_houses_bonus.Contains(loc_current))
+                {
+                    delivered_houses_bonus.Add(loc_current);
+                }
+
+                if (santas_turn)
+                {
+                    loc_santa = loc_current;
+                } else
+                {
+                    loc_robo = loc_current;
+                }
+                santas_turn = !santas_turn;
             }
 
             Console.WriteLine("Houses with At Least One Present: " + delivered_houses.Count);
+            Console.WriteLine("Houses with At Least One Present (Robo): " + delivered_houses_bonus.Count);
 
             Console.ReadLine();
         }
